47b5672be758eb08e32eab6b337ebaa3a49f74ef
66160245 <66160245@go.buu.ac.th>
2025-02-07 01:11:07 +0700

    successed

--- script.js
+++ script.js
@@ -41,21 +41,31 @@ function checkTimeConflict(id, date, startTime, endTime) {
     );
 }
 
-function cancelAppointment(id){
+function cancelAppointment(id) {
     let newAppointment = JSON.parse(localStorage.getItem("appointData")) || [];
     newAppointment = newAppointment.map(app =>
         app.id === id ? { ...app, status: "cancelled" } : app
     );
 
-    localStorage.setItem("appointData",JSON.stringify(newAppointment));
+    localStorage.setItem("appointData", JSON.stringify(newAppointment));
 
     showData();
 
 }
 
-function getUpcomingAppointments(){
+function getUpcomingAppointments() {
     let today = new Date().toISOString().split("T")[0];
     return (JSON.parse(localStorage.getItem("appointData")) || []).filter(app => app.date >= today);
+}
+
+function clearCancel(id) {
+    let clear = JSON.parse(localStorage.getItem("appointData")) || [];
+
+    let newData = clear.filter(data => data.status !== "cancel" && data.status !== "cancelled");
+
+    localStorage.setItem("appointData", JSON.stringify(newData));
+
+    showData();
 }
 
 function showData() {
@@ -66,38 +76,41 @@ function showData() {
     let now = new Date();
     let currentDate = now.toISOString().split("T")[0];
     let currentTime = now.toTimeString().split(" ")[0].substring(0, 5);
-    list = list.filter(app => 
+    list = list.filter(app =>
         app.date > currentDate || (app.date === currentDate && app.endTime > currentTime)
     );
     list.sort((a, b) => {
         if (a.date !== b.date) {
-            return a.date.localeCompare(b.date); 
+            return a.date.localeCompare(b.date);
         }
-        return a.startTime.localeCompare(b.startTime); 
+        return a.startTime.localeCompare(b.startTime);
     });
 
     list.forEach((appointment) => {
-        const appList = document.createElement("li");
+        const appList = document.createElement("div");
 
-        const checkTime = checkTimeConflict(appointment.id,appointment.date,appointment.startTime,appointment.endTime);
-        
+        const checkTime = checkTimeConflict(appointment.id, appointment.date, appointment.startTime, appointment.endTime);
+
         appList.style.color = checkTime ? "red" : "black";
-
-        const cancelled = appointment.status === "cancelled" ? "text-decoration: line-through;" : "";
+        const cancelled = appointment.status === "cancelled" ? "line-through text-gray-400" : "";
 
 
         appList.innerHTML = `
+        <div id="show-box" style="${cancelled}">
             <p><strong>TITLE:</strong> ${appointment.title}</p>
             <p><strong>DATE:</strong> ${appointment.date}</p>
-            <p><strong>START TIME:</strong> ${appointment.startTime}</p>
+            <p<strong>START TIME:</strong> ${appointment.startTime}</p>
             <p><strong>END TIME:</strong> ${appointment.endTime}</p>
             <p><strong>STATUS :</strong> ${appointment.status}</p>
-            <button onclick=" cancelAppointment('${appointment.id}')">CANCLE</button>
+            <button id="cancel" onclick=" cancelAppointment('${appointment.id}')">CANCEL</button>
             <hr>
+            </div>
         `;
         appContrainer.appendChild(appList);
     });
 
 
 }
+
+showData();
 showData(getUpcomingAppointments());
